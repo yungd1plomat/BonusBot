b8b9a37ffcb23dc970dcb441b65f8b2fe716143e
yungd1plomat <aynur.musin.06@mail.ru>
2023-01-09 17:17:47 +0500

    read token as argument

--- BonusBot/Program.cs
+++ BonusBot/Program.cs
@@ -5,9 +5,14 @@ using BonusBot.Core.Data;
 public class Program
 {
     
-    static async Task Main()
+    static async Task Main(string[] args)
     {
-        string token = File.ReadAllText("token.txt");
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Enter token as argument, Example dotnet BonusBot.dll 123456:asdhsadsadhkjhwejquiahdak");
+            return;
+        }
+        string token = args[0];
         IBot bot = new Bot(token);
         await bot.Start();
         Console.ReadLine();
